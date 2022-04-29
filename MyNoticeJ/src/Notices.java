a428931cedb129e8b0f9e1673c730917f17609e0
Enter55 <SGA4@outlook.de>
2022-04-29 17:58:48 +0200

    Created:
     -Class Notices:
        - Properties:
            -blocks
            -notices
     -Menu

---
+++ MyNoticeJ/src/Notices.java
@@ -0,0 +1,14 @@
+public class Notices {
+
+    public String blocks;
+    public String tasks;
+
+
+    public  String GetBlocks(){
+        return blocks;
+    }
+
+    public void SetBlocks(String blocks){
+        this.blocks = blocks;
+    }
+}
